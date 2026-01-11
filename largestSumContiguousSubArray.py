458e9491a7eb84c99fa0eea6b524a24c39f96570
Myself-SK <mail.shivakumar03@gmail.com>
2026-01-11 07:33:29 +0530

    added largestSumContiguousSubArray Program

---
+++ largestSumContiguousSubArray.py
@@ -0,0 +1,17 @@
+l = [-2,-1,-3,4,-1,2,1,-5,4]
+
+def largest_sub_array(arr):
+    max_sum = arr[0]
+    largest_array = arr[0]
+    for i in range(1, len(arr)):
+        step = i
+        for j in range(len(arr)):
+            if sum(arr[j:j+step]) > max_sum:
+                max_sum = sum(arr[j:j+step])
+                largest_array = arr[j:j+step]
+    return max_sum, largest_array
+
+
+max_sum, largest_array = largest_sub_array(l)
+print(max_sum)
+print(largest_array)
